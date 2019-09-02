d9ad1617b9f8176ba4471e73b378f8c566a1fd9c
filipl41 <filipl41@yahoo.com>
2019-09-02 09:28:51 +0200

    drawing of grid added

--- XOXO/app/Main.hs
+++ XOXO/app/Main.hs
@@ -1,6 +1,46 @@
-module Main where
+import Graphics.Gloss (play, white, black, Display(InWindow))
 
-import Lib
+import Graphics.Gloss
+import qualified Data.Map as M
+import Data.IORef
+
+
+type Board = M.Map (Int, Int) Marker
+
+data Marker = X | O | Blank deriving Eq
+
+
+height = 720
+width = 480
+n = 5
 
 main :: IO ()
-main = someFunc
+main = do
+    play (InWindow "Tic-Tac-Toe" (height, width) (300, 300)) white 20 M.empty renderBoard (const id) (const id)
+
+
+cellWidth :: Float
+cellWidth = fromIntegral width / fromIntegral n
+
+cellHeight :: Float
+cellHeight = fromIntegral height / fromIntegral  n
+
+
+renderGrid :: Picture
+renderGrid =  pictures
+    $ concatMap (\i -> [ line [ (i * cellWidth, 0)
+                              , (i * cellWidth, fromIntegral height)
+                              ]
+                       , line [ (0, i * cellHeight)
+                              , (fromIntegral width, i * cellHeight)
+                              ]
+                       ])
+      [0 .. fromIntegral n]
+
+
+
+renderBoard :: Board -> Picture
+renderBoard b = translate (fromIntegral width * (-0.5))
+                          (fromIntegral height * (-0.5))
+                renderGrid     
+               
