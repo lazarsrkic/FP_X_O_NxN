f1d64de885728f33817219526c6454ea4d48bfc5
filipl41 <filipl41@yahoo.com>
2019-09-03 09:36:50 +0200

    drawing of x and o added

--- XOXO/app/Main.hs
+++ XOXO/app/Main.hs
@@ -1,22 +1,27 @@
-import Graphics.Gloss (play, white, black, Display(InWindow))
+import Graphics.Gloss (play, white, black, red, blue, Display(InWindow))
 
-import Graphics.Gloss
+--import Graphics.Gloss
 import qualified Data.Map as M
 import Data.IORef
+import Graphics.Gloss.Data.Picture
+    (Picture, rectangleSolid, thickCircle, translate, pictures, color, rotate, blank, line)
 
+import Graphics.Gloss.Interface.Pure.Game
+    (Event (EventKey), KeyState (Up), MouseButton (LeftButton),
+     Key (MouseButton))
 
-type Board = M.Map (Int, Int) Marker
+type Board = M.Map (Int, Int) Player
 
-data Marker = X | O | Blank deriving Eq
+data Player = X | O | Blank deriving Eq
 
 
 height = 720
 width = 480
-n = 5
+n = 3
 
 main :: IO ()
 main = do
-    play (InWindow "Tic-Tac-Toe" (height, width) (300, 300)) white 20 M.empty renderBoard (const id) (const id)
+    play (InWindow "Tic-Tac-Toe" (height, width) (300, 300)) white 20 M.empty renderBoard nextState (const id)
 
 
 cellWidth :: Float
@@ -25,6 +30,13 @@ cellWidth = fromIntegral width / fromIntegral n
 cellHeight :: Float
 cellHeight = fromIntegral height / fromIntegral  n
 
+renderPlayer :: Player -> Picture
+renderPlayer X = color red $ rotate 45 $ pictures [rectangleSolid 90 10, rectangleSolid 10 90]
+renderPlayer O = color blue $ thickCircle 35 10
+renderPlayer _ = blank
+
+getPlayer :: Board -> (Int, Int) -> Player
+getPlayer = flip $ M.findWithDefault Blank
 
 renderGrid :: Picture
 renderGrid =  pictures
@@ -41,6 +53,18 @@ renderGrid =  pictures
 
 renderBoard :: Board -> Picture
 renderBoard b = translate (fromIntegral width * (-0.5))
-                          (fromIntegral height * (-0.5))
-                renderGrid     
-               
+                          (fromIntegral height * (-0.5)) 
+                $   pictures $ [translate (fromIntegral y  * cellWidth + cellWidth * 0.5) (fromIntegral x  * cellHeight + cellHeight * 0.5)  $renderPlayer $ getPlayer b (x, y) | x <- [0..n], y <- [0..n] ] ++ [renderGrid] 
+  
+
+nextState (EventKey (MouseButton LeftButton) Up _ pos) b =
+    if getPlayer b (toCellCoord pos) == Blank 
+    then M.insert (toCellCoord pos) X b
+    else b
+nextState _ b = b    
+
+currentPlayer :: Board -> Player
+currentPlayer b = X
+    
+toCellCoord :: (Float, Float) -> (Int, Int)
+toCellCoord (x, y) = (floor (( y + (fromIntegral height * 0.5)) / cellHeight), floor((x + (fromIntegral width * 0.5)) / cellWidth))                 
