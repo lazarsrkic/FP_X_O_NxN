a80f04920a9c003d9d7fa9efbaa7f59476c5c2e6
Srdjan Lazarevic <slazarevic1995@gmail.com>
2019-09-15 02:35:03 +0200

    Add start and end screens and event handlers

--- XOXO/app/Main.hs
+++ XOXO/app/Main.hs
@@ -1,109 +1,159 @@
-import Graphics.Gloss (play, white, black, red, blue, Display(InWindow))
+import Graphics.Gloss
 
+import Graphics.Gloss.Data.Color
+
+import Graphics.Gloss.Data.Display
+
+import Graphics.Gloss.Data.Picture
+
+-- import Graphics.Gloss.Interface.Pure.Game
+
+import Graphics.Gloss.Game
 
 import qualified Data.Map as M
 import Data.IORef
-import Graphics.Gloss.Data.Picture
-    (Picture, rectangleSolid, thickCircle, translate, pictures, color, rotate, blank, line)
-import Graphics.Gloss.Interface.Pure.Game
-    (Event (EventKey), KeyState (Up), MouseButton (LeftButton),
-     Key (MouseButton))
 
+-- import Graphics.Gloss.Juicy
 type Board = M.Map (Int, Int) Player
 
-data Player = X | O | Blank deriving Eq
+data Player = X | O | BlankP deriving Eq
 
-data GameState = Running | GameOver  deriving Eq
+data GameState = StartUp | Running | GameOver | WonX | WonO deriving Eq
 
-
-height = 480
-width = 480
-n = 3
--- FIXME
--- n :: IO Int
--- n = do
---   putStrLn "enter grid dimension: "
---   input1 <- getLine
---   let x = (read input1 :: Int)
---   return (x)
+data State = State { gamestate :: GameState
+                   , boardDim  :: Int
+                   , board     :: Board
+                   , gridDim   :: Int
+                   }
 
 main :: IO ()
-main =  play (InWindow "Tic-Tac-Toe" (height, width) (300, 300)) black 20 M.empty renderBoard nextState (const id)
+main = let size       = (600,600)
+           window     =  (InWindow "Tic-Tac-Toe" size (300, 300))
+           background = black
+           fps        = 15
+       in Graphics.Gloss.Game.play  
+          window
+          background
+          fps
+          initState
+          render 
+          nextEvent 
+          []
+
+render :: State -> Picture
+render state = let currstate = gamestate state
+               in case currstate of
+                  StartUp -> startScreen (gridDim state, gridDim state)
+                  GameOver -> gameoverScreen (gridDim state, gridDim state)
+                  WonX    ->  wonXScreen (gridDim state, gridDim state)
+                  WonO    ->  wonOScreen (gridDim state, gridDim state)
+                  Running -> renderTable state
+
+picStart = png "img/start.png"
+picGameover = png "img/gameover.png"
+picWonX = png "img/wonx.png"
+picWonO = png "img/wono.png"
+
+gameoverScreen windowSize = useImage picGameover windowSize
+startScreen windowSize = useImage picStart windowSize
+wonXScreen windowSize = useImage picWonX windowSize
+wonOScreen windowSize = useImage picWonO windowSize
+
+useImage :: Picture -> (Int,Int) -> Picture
+useImage picture windowSize =
+  let (_, (picWidth, picHeight)) = boundingBox picture
+      (winWidth,winHeight)     = (fromIntegral $ fst windowSize, fromIntegral $ snd windowSize)
+      scaleW                   = winWidth / picWidth
+      scaleH                   = winHeight / picHeight
+      scaleFactor              = max scaleH scaleW
+  in scale scaleFactor scaleFactor $ picture
 
 
-cellWidth :: Float
-cellWidth = fromIntegral width / fromIntegral n
+initState = State { gamestate = StartUp
+                  , boardDim  = 3
+                  , board     = M.empty
+                  , gridDim   = 540
+                  }
 
-cellHeight :: Float
-cellHeight = fromIntegral height / fromIntegral  n
+movesOnBoard :: State -> [(Int, Int)]
+movesOnBoard state = [(x, y) | x <- [0..(boardDim state)-1], y <- [0..(boardDim state)-1], getPlayer (board state) (x, y) == BlankP]
+    
+cellCoord:: (Float,Float) -> State -> (Int,Int)
+cellCoord (x,y) state = (floor (( y + (fromIntegral (gridDim state) * 0.5)) / (cellSize state)), floor ((x + (fromIntegral (gridDim state) * 0.5)) / (cellSize state)))
 
-renderPlayer :: Player -> Picture
-renderPlayer X = color red $ rotate 45 $ pictures [rectangleSolid (270 / fromIntegral n) (45 / fromIntegral n), rectangleSolid (45 / fromIntegral n) (270 / fromIntegral n)]
-renderPlayer O = color blue $ thickCircle (105 / fromIntegral n) (45 / fromIntegral n)
-renderPlayer _ = blank
+calculatePlayerTurnFromState pos state = if getPlayer (board state) (cellCoord pos state) == BlankP && currPlayer state == X
+                                              then M.insert (cellCoord pos state) X (board state)
+                                         else if getPlayer (board state) (cellCoord pos state) == BlankP && currPlayer state == O
+                                              then M.insert (cellCoord pos state) O (board state)
+                                         else M.insert (cellCoord pos state) BlankP (board state)
 
-getPlayer :: Board -> (Int, Int) -> Player
-getPlayer = flip $ M.findWithDefault Blank
+currPlayer :: State -> Player
+currPlayer state = if mod (boardDim state) 2 == 1 then if odd $ length $ movesOnBoard state  then  X else O
+                  else if odd $ length $ movesOnBoard state  then  O else X
 
-renderGrid :: Board -> Picture
-renderGrid b = color (outcomeColor b)  $ pictures
-    $ concatMap (\i -> [ line [ (i * cellWidth, 0)
-                              , (i * cellWidth, fromIntegral height)
+currGameState state | (winnerFromState state) == Just X = WonX
+                    | (winnerFromState state) == Just O = WonO
+                    | length (movesOnBoard state) == 0 = GameOver
+                    | (gamestate state) == StartUp = StartUp
+                    | otherwise =  Running
+
+nextEvent :: Event -> State -> State
+nextEvent (EventKey (MouseButton LeftButton) Up _ pos) state = if (currGameState state) == Running then state {gamestate = Running, board = calculatePlayerTurnFromState pos state}
+                                                              else if (currGameState state) == WonX  then state {gamestate = WonX, board = M.empty}
+                                                              else if (currGameState state) == WonO  then state {gamestate = WonO, board = M.empty}
+                                                              else if (currGameState state) == GameOver then state {gamestate = GameOver}
+                                                              else state
+
+nextEvent (EventKey (SpecialKey KeySpace) Down _ _) state = if (currGameState state) == StartUp then state {gamestate = Running, boardDim = 3}
+                                                              -- else if (currGameState state) == WonX then state {gamestate = Running, board = M.empty}
+                                                              -- else if (currGameState state) == WonO then state {gamestate = Running, board = M.empty}
+                                                              else if (currGameState state) == GameOver then state {gamestate = Running, board = M.empty}
+                                                              else state
+
+nextEvent (EventKey (Char '3') Down _ _) state = if currGameState state == StartUp then state { gamestate = Running, boardDim = 3 } else state
+nextEvent (EventKey (Char '4') Down _ _) state = if currGameState state == StartUp then state { gamestate = Running, boardDim = 4 } else state
+nextEvent (EventKey (Char '5') Down _ _) state = if currGameState state == StartUp then state { gamestate = Running, boardDim = 5 } else state
+nextEvent (EventKey (Char '6') Down _ _) state = if currGameState state == StartUp then state { gamestate = Running, boardDim = 6 } else state
+nextEvent (EventKey (Char '7') Down _ _) state = if currGameState state == StartUp then state { gamestate = Running, boardDim = 7 } else state
+nextEvent (EventKey (Char '8') Down _ _) state = if currGameState state == StartUp then state { gamestate = Running, boardDim = 8 } else state
+nextEvent (EventKey (Char '9') Down _ _) state = if currGameState state == StartUp then state { gamestate = Running, boardDim = 9 } else state
+nextEvent _ state = state
+
+--nova implementacija
+
+cellSize :: State -> Float
+cellSize state = fromIntegral (gridDim state) / fromIntegral (boardDim state)
+
+winnerFromState :: State -> Maybe Player
+winnerFromState state = getMaybeValue .map head .filter (\xs -> head xs /= BlankP && allSame xs) . map (map $ getPlayer (board state)) $ possibleMove (boardDim state, boardDim state) (boardDim state)
+
+outcomeColorFromState :: State -> Color
+outcomeColorFromState state = if winnerFromState state == Just X then red 
+                              else if winnerFromState state == Just O then blue
+                              else white
+
+renderGridFromState :: State ->Picture
+renderGridFromState state = color (outcomeColorFromState state) $ pictures
+    $ concatMap (\i -> [ line [ (i * (cellSize state), 0)
+                              , (i * (cellSize state), fromIntegral (gridDim state))
                               ]
-                       , line [ (0, i * cellHeight)
-                              , (fromIntegral width, i * cellHeight)
+                       , line [ (0, i * (cellSize state))
+                              , (fromIntegral (gridDim state), i * (cellSize state))
                               ]
                        ])
-      [0 .. fromIntegral n]
+      [0 .. fromIntegral (boardDim state)]
+
+renderTable :: State -> Picture
+renderTable state = translate (fromIntegral (gridDim state) * (-0.5)) (fromIntegral (gridDim state) * (-0.5))
+                    $ pictures 
+                    $ [translate (fromIntegral y  * (cellSize state) + (cellSize state) * 0.5) (fromIntegral x  * (cellSize state) + (cellSize state) * 0.5)  $renderPlayers (boardDim state) (getPlayer (board state) (x, y)) | x <- [0..(boardDim state) - 1], y <- [0..(boardDim state) - 1] ] ++ [renderGridFromState state ] 
 
 
 
-renderBoard :: Board -> Picture
-renderBoard b = translate (fromIntegral width * (-0.5))
-                          (fromIntegral height * (-0.5)) 
-                 $ pictures $ [translate (fromIntegral y  * cellWidth + cellWidth * 0.5) (fromIntegral x  * cellHeight + cellHeight * 0.5)  $renderPlayer $ getPlayer b (x, y) | x <- [0..n - 1], y <- [0..n - 1] ] ++ [renderGrid b ] 
-  
-
-nextState (EventKey (MouseButton LeftButton) Up _ pos) b = if currentGameState b == Running then  calculatePlayerTurn pos b
-                                                                                          else M.empty 
-nextState _ b = b    
-
-
-calculatePlayerTurn pos b =  if getPlayer b (toCellCoord pos) == Blank && currentPlayer b == X
-            then M.insert (toCellCoord pos) X b
-    else if  getPlayer b (toCellCoord pos) == Blank && currentPlayer b == O
-            then M.insert (toCellCoord pos) O b 
-
-    else b 
-
-
-currentPlayer :: Board -> Player
-currentPlayer b = if  mod n 2 == 1 then if odd $ length $ movesOnBoard b  then  X else O
-                  else if odd $ length $ movesOnBoard b  then  O else X
-                  
-movesOnBoard :: Board -> [(Int, Int)]
-movesOnBoard b = [(x, y) | x <- [0..n-1], y <- [0..n-1], getPlayer b (x, y) == Blank]
-    
-toCellCoord :: (Float, Float) -> (Int, Int)
-toCellCoord (x, y) = (floor (( y + (fromIntegral height * 0.5)) / cellHeight), floor((x + (fromIntegral width * 0.5)) / cellWidth))                 
-
-
-currentGameState b   |  Just player <- winner b  =  GameOver
-                     |length ( movesOnBoard b) == 0 =  GameOver
-                     |otherwise =  Running
-                   
-                   
-
-
-getMaybeValue (x:_) = Just x
-getMaybeValue _ = Nothing
-
-allSame []     = True
-allSame (x:xs) = all (==x) xs
-
-winner :: Board -> Maybe Player
-winner b = getMaybeValue .map head .filter (\xs -> head xs /= Blank && allSame xs) . map (map $ getPlayer b) $ possibleMoves (n, n) 
-
+renderPlayers :: Int ->Player -> Picture
+renderPlayers dim X = color red $ rotate 45 $ pictures [rectangleSolid (270 / fromIntegral dim) (45 / fromIntegral dim), rectangleSolid (45 / fromIntegral dim) (270 / fromIntegral dim)]
+renderPlayers dim O = color blue $ thickCircle (105 / fromIntegral dim) (45 / fromIntegral dim)
+renderPlayers dim _ = blank
 
 step :: [(Int, Int) -> (Int, Int)]
 step = [ (\(x, y) -> (x+1, y  ))
@@ -112,11 +162,15 @@ step = [ (\(x, y) -> (x+1, y  ))
              , (\(x, y) -> (x+1, y-1))
              ]
 
-possibleMoves (x, y)  =
+getMaybeValue (x:_) = Just x
+getMaybeValue _ = Nothing
+
+allSame []     = True
+allSame (x:xs) = all (==x) xs
+
+possibleMove (x, y) dim =
     filter (all (\(i, j) -> i >= 0 && j >= 0 && i < x && j < y))
-    [take n (iterate d (i, j)) | i <- [0..x-1], j <- [0..y-1], d <- step]
+    [take dim (iterate d (i, j)) | i <- [0..x-1], j <- [0..y-1], d <- step]
 
-
-outcomeColor b = if winner b == Just X  then red
-                                     else if winner b == Just O then blue
-                                                                else  white
+getPlayer :: Board -> (Int, Int) -> Player
+getPlayer = flip $ M.findWithDefault BlankP
